eeb6e71210b3d8485837e6ac6cc683dbd0bce00b
Azubuike Ibe <x00191169@mytudublin.ie>
2024-02-12 17:26:23 +0000

    Add project files.

---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿namespace JokesApp.Models
+{
+    public class Joke
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestions { get; set; }
+
+        public string JokesAnswer { get; set; }
+
+        public Joke()
+        {
+            
+        }
+    }
+}
